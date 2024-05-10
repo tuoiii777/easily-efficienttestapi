65b834bbb658d904000778aa59aac00d73ac0471
tuoiii777 <azooz2007apun0suy@hotmail.com>
2024-05-10 18:25:38 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-function isSymmetric(root) {
-  if (!root) return true;
-  return isMirror(root.left, root.right);
-  function isMirror(left, right) {
-    if (!left && !right) return true;
-    if (!left || !right || left.val !== right.val) return false;
-    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
-  }
+function sayHi() {
+  console.log("Hi!");
 }
+
+setTimeout(sayHi, 2000); // Run sayHi after 2 seconds
