71d9116a2f54cef6e4a1d14d0a81ef4c34c455ce
tuoiii777 <azooz2007apun0suy@hotmail.com>
2024-05-13 02:25:33 +0700

    update aptos

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function isValidBST(root) {
-  return isValid(root, null, null);
-}
-function isValid(node, min, max) {
-  if (!node) return true;
-  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
-    return false;
-  return (
-    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
-  );
-}
+const colors = ["red", "green", "blue"];
+const randomColor = colors[Math.floor(Math.random() * colors.length)];
+console.log(randomColor);
