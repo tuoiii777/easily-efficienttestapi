bbcdd9284d9dfa4a1a6d7259c2dc5ce2dae7e767
tuoiii777 <azooz2007apun0suy@hotmail.com>
2024-05-14 04:33:18 +0700

    feat task

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const colors = ["red", "green", "blue"];
-const randomColor = colors[Math.floor(Math.random() * colors.length)];
-console.log(randomColor);
+function lowestCommonAncestor(root, p, q) {
+  if (!root || root === p || root === q) return root;
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+  if (left && right) return root;
+  return left ? left : right;
+}
